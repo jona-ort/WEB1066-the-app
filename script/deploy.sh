8cc55eaa98598a89f9eeadc3821a33a6beb54f78
jona-ort <jona160@gmail.com>
2020-05-07 18:26:08 -0400

    Final Project #10 Deployment
    Add messages to deploy script.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+echo "login..."
 echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USER} --password-stdin
+echo "tag..."
 docker tag the_app jonaort/gcp-training-ci-1:$(git rev-parse HEAD)
+echo "push..."
 docker push jonaort/the-app:$(git rev-parse HEAD)
+echo "done"
